962695b81bf069f737cc274c56575495739f2980
Richard Palmer <richeytastic@gmail.com>
2019-06-18 20:27:48 +0800

    Added functions for getting RAM availability and use.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Prerequisites
 - [Boost](http://www.boost.org/) 1.68+ (reqs. building of shared libs)
 - [Eigen3](http://eigen.tuxfamily.org)
+
+The Memory module contains open source code by [David Robert Nadeau](http://NadeauSoftware.com/)
+made available under the [Creative Commons Attribution 3.0 Unported License](http://creativecommons.org/licenses/by/3.0/deed.en_US).
---
+++ include/Memory.h
@@ -0,0 +1,46 @@
+/************************************************************************
+ * Copyright (C) 2019 Richard Palmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+
+#ifndef RLIB_MEMORY_H
+#define RLIB_MEMORY_H
+
+#include "rlib_Export.h"
+#include <cstdlib>
+
+namespace rlib {
+
+/**
+ * Returns the size of physical memory (RAM) in bytes.
+ */
+rlib_EXPORT size_t getMemorySize();
+
+
+/**
+ * Returns maximum so far resident set size (physical memory use) in
+ * bytes or zero if the value cannot be determined for this OS.
+ */
+rlib_EXPORT size_t getPeakRSS();
+
+/**
+ * Returns the current resident set size (physical memory use in
+ * bytes or zero if the value cannot be determined for this OS.
+ */
+rlib_EXPORT size_t getCurrentRSS();
+
+}	// end namespace
+
+#endif
