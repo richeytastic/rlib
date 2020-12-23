a9dbb3b005f3ed2a8b09ad4440b5a2b82f32e313
Richard Palmer <richeytastic@gmail.com>
2020-12-23 15:22:38 +0800

    Added MultiLock

---
+++ include/rlib/MultiLock.h
@@ -0,0 +1,62 @@
+/************************************************************************
+ * Copyright (C) 2020 Richard Palmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+
+#ifndef RLIB_MULTILOCK_H
+#define RLIB_MULTILOCK_H
+
+#ifdef _WIN32
+#pragma warning( disable : 4251)
+#endif
+
+#include "rlib_Export.h"
+#include <unordered_set>
+
+namespace rlib {
+
+class rlib_EXPORT MultiLock
+{
+public:
+    MultiLock();
+
+    // Remove all keys/locks (unlocking).
+    void reset();
+
+    // Add a key without locking.
+    int addKey();
+
+    // Lock with a new key and return it.
+    int lock();
+
+    // Lock with an existing key (returns true iff key exists).
+    bool lock( int key);
+
+    // Use the given key. Key is discarded from being available to
+    // use again if discardKey is set true. Will not necessarily
+    // unlock if there are remaining keys still locking.
+    void turnkey( int key, bool discardKey=false);
+
+    inline bool isLocked() const { return !_locks.empty();}
+
+private:
+    int _maxKey;
+    std::unordered_set<int> _keys;
+    std::unordered_set<int> _locks;
+};  // end class
+
+}	// end namespace
+
+#endif
--- src/MultiLock.cpp
+++ src/MultiLock.cpp
@@ -1,5 +1,5 @@
 /************************************************************************
- * Copyright (C) 2019 Richard Palmer
+ * Copyright (C) 2020 Richard Palmer
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,29 +15,51 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
 
-#include <string>
-#include <algorithm>
-#include <iostream>
+#include <MultiLock.h>
+using rlib::MultiLock;
 
-#include "rlib/ConvexOptimiser.h"
-#include "rlib/Convert.h"
-#include "rlib/CmdLnArgsParser.h"
-#include "rlib/CpuTimer.h"
-#include "rlib/Exceptions.h"
-#include "rlib/FileIO.h"
-#include "rlib/LinearRegressor.h"
-#include "rlib/MathUtil.h"
-#include "rlib/Memory.h"
-#include "rlib/MultiLock.h"
-#include "rlib/NewtonRaphson.h"
-#include "rlib/Pack.h"
-#include "rlib/Profiling.h"
-#include "rlib/ProgressDelegate.h"
-#include "rlib/ProgressUpdater.h"
-#include "rlib/Random.h"
-#include "rlib/RangedScalarDistribution.h"
-#include "rlib/Round.h"
-#include "rlib/SymmetricBitSet.h"
-#include "rlib/SymmetricMatrix.h"
-#include "rlib/Timer.h"
-#include "rlib/Vectors.h"
+
+MultiLock::MultiLock() : _maxKey(0)
+{
+}   // end ctor
+
+
+void MultiLock::reset()
+{
+    _maxKey = 0;
+    _keys.clear();
+    _locks.clear();
+}   // end reset
+
+
+int MultiLock::addKey()
+{
+    _maxKey++;
+    _keys.insert(_maxKey);
+    return _maxKey;
+}   // end addKey
+
+
+int MultiLock::lock()
+{
+    const int nkey = addKey();
+    _locks.insert(nkey);
+    return nkey;
+}   // end lock
+
+
+bool MultiLock::lock( int key)
+{
+    const bool gotkey = _keys.count(key) > 0;
+    if ( gotkey)
+        _locks.insert(key);
+    return gotkey;
+}   // end lock
+
+
+void MultiLock::turnkey( int key, bool dkey)
+{
+    _locks.erase(key);
+    if ( dkey && _keys.count(key) > 0)
+        _keys.erase(key);
+}   // end turnkey
